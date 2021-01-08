b1b6461fe2dea4d3b9d5762b71296be808504c95
jsohnfile <hoodieee@Jaes-MacBook-Air.local>
2021-01-08 15:58:05 -0700

    update

--- src/Contact.js
+++ src/Contact.js
@@ -8,17 +8,17 @@ import "./Contact.css";
 
 function Contact(){
     return (
-        <div className="contact" id="contact">
         <Fade bottom>
-            <h3>Co<span className="contact__span">n</span>tact Me</h3>
-                <div className="contact__container">
-                    <a href="mailto:jsohnfile@gmail.com" target="_blank" title="Email Me"><EmailOutlined style={{fontSize:'40px'}} className="contact__icon"/></a>
-                    <a href="https://www.github.com/jsohnfile" target="_blank" title="GitHub"><GitHubIcon style={{fontSize:'40px'}}  className="contact__icon"/></a>
-                    <a href="https://www.linkedin.com/in/jaesohn" target="_blank" title="LinkedIn"><LinkedInIcon style={{fontSize:'40px'}}  className="contact__icon"/></a>
-                    <a href="https://docs.google.com/document/d/1Ct0s_fd66HzVxcWiXmQAB6RqflwLMhkcQPHduVbhqTI/edit?usp=sharing" target="_blank" title="Resume"><InsertDriveFileIcon style={{fontSize: '40px'}} className="contact__icon"/></a>
-                </div>
+            <div className="contact" id="contact">
+                <h3>Co<span className="contact__span">n</span>tact Me</h3>
+                    <div className="contact__container">
+                        <a href="mailto:jsohnfile@gmail.com" target="_blank" title="Email Me"><EmailOutlined style={{fontSize:'40px'}} className="contact__icon"/></a>
+                        <a href="https://www.github.com/jsohnfile" target="_blank" title="GitHub"><GitHubIcon style={{fontSize:'40px'}}  className="contact__icon"/></a>
+                        <a href="https://www.linkedin.com/in/jaesohn" target="_blank" title="LinkedIn"><LinkedInIcon style={{fontSize:'40px'}}  className="contact__icon"/></a>
+                        <a href="https://docs.google.com/document/d/1Ct0s_fd66HzVxcWiXmQAB6RqflwLMhkcQPHduVbhqTI/edit?usp=sharing" target="_blank" title="Resume"><InsertDriveFileIcon style={{fontSize: '40px'}} className="contact__icon"/></a>
+                    </div>
+            </div>
         </Fade>
-        </div>
     )
 }
 
